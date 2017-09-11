cdecd7ddbab073cf7cd74a36ac853795d9b4b6ad
Jay Cribas <feverjay@gmail.com>
2017-09-11 13:56:55 -0700

    Modal opens and closes

--- part-3/hotel.js
+++ part-3/hotel.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(event){
-  console.log('yay!')
+  const modal = document.getElementById('modal')
+  const bookButtons = document.getElementsByClassName('book-btn')
+  const closeModal = document.getElementById('close')
+
+  // Open modal
+  for(let bookBtn of bookButtons){
+    bookBtn.addEventListener('click', function(event){
+      modal.style.display = 'block'
+    })
+  }
+
+  // Close modal
+  closeModal.addEventListener('click', function(event){
+    modal.style.display = 'none'
+  })
+
 })
